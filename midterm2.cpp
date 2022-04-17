78856e0e496e95abb09057259b8e7bb910123d44
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-17 12:45:05 -0700

    submit

---
+++ midterm2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream ifs;
+    int score1, socre2;
+    int sum;
+    string sname;
+    float avg;
+
+    for (int i=0; i<=10; i++)
+    
+}
