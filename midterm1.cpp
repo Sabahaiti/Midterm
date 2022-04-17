69472deb5701582432ae1dbab618912a28134b5e
Sabahaiti <sxahebaier@zonemail.clpccd.edu>
2022-04-17 12:36:35 -0700

    submit

--- midterm1.cpp
+++ midterm1.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main()
 {
-    
+    const  int N=5, M=5;
+    int  i, j;
+    for(i=0;i<5;i++) 
+    {   for(j=i; j<5-i; j++)
+            cout << " (" << i << "," << j << ") " ;
+        cout << endl;
+    }
 }
